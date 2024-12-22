a8588f5c9d9fd673e1209eb6f2086fe931b70184
Kaminoyu <133680855+Kaminoyu@users.noreply.github.com>
2024-12-22 10:57:26 +0700

    Update SettingAccessor.h

--- SettingAccessor.h
+++ SettingAccessor.h
@@ -1,69 +1,127 @@
+``` cpp
 #pragma once
+#ifndef SETTING_ACCESSOR_H
+#define SETTING_ACCESSOR_H
 
 namespace Stelo {
-    template<typename T, typename CLASS, T&(*FuncGet)(CLASS* obj),
-        void (*FuncSet)(CLASS* obj, const T& new_value)>
+    template<typename T, typename CLASS, T& (*FuncGet)(CLASS* ref),
+        void (*FuncSet)(CLASS* ref, const T& value)>
     class SettingAccessor {
     public:
         CLASS* class_ptr;
-        T data; 
+        T data;
 
-        SettingAccessor(CLASS* obj, const T& intial) : 
-            class_ptr(obj), data(intial) {}
+        SettingAccessor(CLASS* ref, const T& intial) :
+            class_ptr(ref), data(intial) {
+        }
 
-        // getter oprate
-        T& operator()() {
-            return FuncGet ? FuncGet(class_ptr) : data;
+        // getter operate
+        T operator()() {
+            return FuncGet ? *FuncGet(class_ptr) : data;
         }
         T& operator->() {
             return FuncGet ? FuncGet(class_ptr) : data;
         }
-        T& get() {
-            return FuncGet ? FuncGet(class_ptr) : data;
+        T get() {
+            return FuncGet ? *FuncGet(class_ptr) : data;
         }
 
-        // setter oprate
-        void set(const T& new_value) {
+        // setter operate
+        void set(const T& value) {
             if (FuncSet) {
-                FuncSet(class_ptr, new_value);
+                FuncSet(class_ptr, value);
             }
             else {
-                data = new_value;
+                data = value;
             }
         }
-        T& operator=(const T& new_value) {
+        T operator=(const T& value) {
             if (FuncSet) {
-                FuncSet(class_ptr, new_value);
+                FuncSet(class_ptr, value);
             }
             else {
-                data = new_value;
+                data = value;
             }
             return data;
         }
 
-        T& operator+(const T& value) {
-            return FuncGet ? FuncGet(class_ptr) : data + value;
+        // arithmetic operators
+        T operator+(const T& value) const {
+            return FuncGet ? FuncGet(class_ptr) + value : data + value;
         }
 
-        T& operator-(const T& value) {
-            return FuncGet ? FuncGet(class_ptr) : data - value;
+        T operator-(const T& value) const {
+            return FuncGet ? FuncGet(class_ptr) - value : data - value;
         }
 
-        T& operator*(const T& value) {
-            return FuncGet ? FuncGet(class_ptr) : data * value;
+        T operator*(const T& value) const {
+            return FuncGet ? FuncGet(class_ptr) * value : data * value;
         }
 
-        T& operator/(const T& value) {
-            return FuncGet ? FuncGet(class_ptr) : data / value;
+        T operator/(const T& value) const {
+            return FuncGet ? FuncGet(class_ptr) / value : data / value;
+        }
+
+        // compound assignment operators
+        SettingAccessor& operator+=(const T& value) {
+            if (FuncSet) {
+                FuncSet(class_ptr, data + value);
+            }
+            else data += value;
+            return *this;
+        }
+
+        SettingAccessor& operator-=(const T& value) {
+            if (FuncSet) {
+                FuncSet(class_ptr, data - value) ;
+            }
+            else data -= value;
+
+            return *this;
+        }
+
+        SettingAccessor& operator*=(const T& value) {
+            if (FuncSet) {
+                FuncSet(class_ptr, data * value);
+            }
+            else data *= value;
+
+            return *this;
+        }
+
+        SettingAccessor& operator/=(const T& value) {
+            if (FuncSet) {
+                FuncSet(class_ptr, data / value);
+            }
+            else data /= value;
+
+            return *this;
         }
     };
 
-#define Setting(T, name_variable, CLASS, func_get, func_set, initial_value) \
+#define Setting(T, name_variable, CLASS, getter, setter, initial_value) \
 private: \
-    static T& get_##name_variable(CLASS* obj) func_get \
-    static void set_##name_variable(CLASS* obj, const T& new_value) func_set \
+    static T& get_##name_variable(CLASS* ref) getter \
+    static void set_##name_variable(CLASS* ref, const T& value) setter\
 public: \
     Stelo::SettingAccessor< \
-        T, CLASS, &CLASS::get_##name_variable, &CLASS::set_##name_variable \
-    > name_variable{ this, initial_value };
+        T, CLASS, &CLASS::get_##name_variable, &CLASS::set_##name_variable\
+    > name_variable{ this, initial_value }
+
+#define SettingGetterDefault(T, name_variable, CLASS, setter, initial_value) \
+private: \
+    static void set_##name_variable(CLASS* ref, const T& value) setter \
+public: \
+    Stelo::SettingAccessor<T, CLASS, nullptr, &CLASS::set_##name_variable\
+    > name_variable{ this, initial_value }
+
+#define SettingSetterDefault(T, name_variable, CLASS, getter, initial_value) \
+private: \
+    static T& get_##name_variable(CLASS* ref) getter \
+public: \
+    Stelo::SettingAccessor< \
+        T, CLASS, &CLASS::get_##name_variable, nullptr_t\
+    > name_variable{ this, initial_value }
 }
+#endif // SETTING_ACCESSOR_HPP
+```
